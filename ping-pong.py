f47e48ce78d0d853bfed005cc59c72a056bf8938
Dmitriy-Efremov <83871821+Dmitriy-Efremov@users.noreply.github.com>
2021-05-15 17:25:39 +0300

    Add files via upload

--- ping-pong.py
+++ ping-pong.py
@@ -8,10 +8,11 @@ num_fire = 0
 rel_time = False
 
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, player_speed, size_x, size_y):
+    def __init__(self, player_image, player_x, player_y,speed_y,speed_x, size_x, size_y):
         super().__init__()
         self.image = transform.scale(image.load(player_image), (size_x, size_y))
-        self.speed = player_speed
+        self.speed_y = speed_y
+        self.speed_x = speed_x
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
@@ -22,18 +23,23 @@ class Player(GameSprite):
     def update(self):
         keys_pressed = key.get_pressed()
         if keys_pressed[K_UP] and self.rect.y > 5:
-            self.rect.y -= self.speed
+            self.rect.y -= self.speed_y
         if keys_pressed[K_DOWN] and self.rect.y < 328:
-            self.rect.y += self.speed
+            self.rect.y += self.speed_y
     def update2(self):
         keys_pressed = key.get_pressed()
         if keys_pressed[K_w] and self.rect.y > 5:
-            self.rect.y -= self.speed
+            self.rect.y -= self.speed_y
         if keys_pressed[K_s] and self.rect.y < 328:
-            self.rect.y += self.speed 
+            self.rect.y += self.speed_y
 class Ball(GameSprite):
     def update(self):
-        pass     
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y   
+        if self.rect.y >=465 or self.rect.y <= 5:
+            self.speed_y = self.speed_y * -1
+        if self.rect.x >= 665 or self.rect.x <=5:
+            self.speed_x = self.speed_x * -1
             
 window = display.set_mode((700,500))
 
@@ -41,9 +47,9 @@ clock = time.Clock()
 FPS = 60
 
 background = transform.scale(image.load("background.jpg"), (700,500))
-player = Player('player.png', 645, 222,5, 50, 170)
-player2 = Player('player.png', 6, 222,5, 50, 170)
-ball = Ball('ball.png',350,250,8,50,50)
+player = Player('player.png', 645, 222,5,0, 50, 170)
+player2 = Player('player.png', 6, 222,5, 0,50, 170)
+ball = Ball('ball.png',350,250,8,8,50,50)
 
 finish = False
 game = True
@@ -55,6 +61,7 @@ while game:
         player2.reset()
         player2.update2()
         ball.reset()
+        ball.update()
     for e in event.get():
         if e.type == QUIT:
             game = False
